2770c35462416d42f11e031b3fab1f87fbf7630f
Burak <115874608+Burakyc@users.noreply.github.com>
2023-02-05 13:56:07 +0300

    Update ödev1.cs

--- ödev1.cs
+++ ödev1.cs
@@ -1,1 +1,44 @@
-<>
+using System;
+
+
+
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          List<int> pçiftsayılar = new List<int>();
+            
+            
+          Console.Write("Kaç tane sayi gireceksiniz: ");
+          int sayi = Convert.ToInt32(Console.ReadLine());
+          for(int i=0 ;i<sayi;i++)
+          {
+            Console.Write("pozitif sayı giriniz: ");
+             int sayilar = Convert.ToInt32(Console.ReadLine());
+             if(sayilar%2==0){
+            pçiftsayılar.Add(sayilar);
+             }
+             
+          }
+
+          Console.Write("Girdiğiniz pozitif çift sayilar : ");
+          foreach(var item in pçiftsayılar)
+          {
+            Console.Write(item+" ");
+          }
+          
+
+        
+                                  
+        }
+           
+        
+    }
+
+   
+   
+      
+}
